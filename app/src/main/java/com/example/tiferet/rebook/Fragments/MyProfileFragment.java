1fca8b4809d74ec65f88bfa515acf6f654cdc752
tifochen <tifcohen@gmail.com>
2016-01-04 14:06:06 +0200

    book shelf error inflating HListView

--- app/src/main/java/com/example/tiferet/rebook/Fragments/MyProfileFragment.java
+++ app/src/main/java/com/example/tiferet/rebook/Fragments/MyProfileFragment.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 
 import com.example.tiferet.rebook.Model.Book;
 import com.example.tiferet.rebook.Model.BookDB;
-import com.example.tiferet.rebook.Model.Post;
 import com.example.tiferet.rebook.Model.User;
 import com.example.tiferet.rebook.Model.UserDB;
 import com.example.tiferet.rebook.R;
@@ -31,6 +30,8 @@ public class MyProfileFragment extends Fragment {
     List<Book> myReadingData;
     ListView myFollowingList;
     List<User> myFollowingData;
+    ListView myBookShelfList;
+    List<Book> myBookShelfData;
 
     MyProfileFragmentDelegate delegate;
     public void setDelegate(MyProfileFragmentDelegate delegate){ this.delegate = delegate;}
@@ -52,6 +53,11 @@ public class MyProfileFragment extends Fragment {
         myFollowingData = UserDB.getInstance().getAllUsers();
         MyFollowingAdapter followingAdapter = new MyFollowingAdapter();
         myFollowingList.setAdapter(followingAdapter);
+
+        myBookShelfList = (ListView) view.findViewById(R.id.myBookShelfList);
+        myBookShelfData = BookDB.getInstance().getAllBooks();
+        MyShelfAdapter bookShelfAdapter = new MyShelfAdapter();
+        myBookShelfList.setAdapter(bookShelfAdapter);
 
         return view;
     }
@@ -122,6 +128,42 @@ public class MyProfileFragment extends Fragment {
             ImageView followingImage = (ImageView) convertView.findViewById(R.id.followingImage);
 
             User user = myFollowingData.get(position);
+            return convertView;
+        }
+    }
+
+    class MyShelfAdapter extends BaseAdapter {
+
+        public MyShelfAdapter() {
+        }
+
+        @Override
+        public int getCount() { //returns the size of the list
+            return myBookShelfData.size();
+        }
+
+        @Override
+        public Object getItem(int position) { //returns the post
+            return myBookShelfData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) { //returns post id
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                convertView = inflater.inflate(R.layout.my_book_shelf_single_column, null);
+            }
+            TextView bookName = (TextView) convertView.findViewById(R.id.bookShelfName);
+            ImageView bookImage = (ImageView) convertView.findViewById(R.id.bookShelfImage);
+
+            Book book = myReadingData.get(position);
+            bookName.setText(book.getBookName());
+
             return convertView;
         }
     }
