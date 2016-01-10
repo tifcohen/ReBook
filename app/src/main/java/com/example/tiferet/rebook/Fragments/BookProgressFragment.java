cad5b80303df77c957cce995549dcccd69467248
alon-abadi <alon.abadi@gmail.com>
2016-01-10 17:10:50 +0200

    MyProfile working - now fix links.

--- app/src/main/java/com/example/tiferet/rebook/Fragments/BookProgressFragment.java
+++ app/src/main/java/com/example/tiferet/rebook/Fragments/BookProgressFragment.java
@@ -14,16 +14,13 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.tiferet.rebook.Model.Book;
-import com.example.tiferet.rebook.Model.BookDB;
 import com.example.tiferet.rebook.Model.Model;
 import com.example.tiferet.rebook.Model.Post;
 import com.example.tiferet.rebook.Model.User;
-import com.example.tiferet.rebook.Model.UserDB;
 import com.example.tiferet.rebook.R;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by TIFERET on 05-Jan-16.
@@ -37,12 +34,15 @@ public class BookProgressFragment extends Fragment {
 
     ListView list;
     ArrayList<Post> data;
+    ArrayList<User> users;
     Book book;
     User curr = new User(ParseUser.getCurrentUser());
+    //User curr = null;
     String userId;
-    TextView bookPages;
+    TextView bookProgressPages;
     ProgressBar bookProgress;
     RelativeLayout bookProgressLayout;
+    TextView bookProfileTitle;
 
     BookProgressFragmentDelegate delegate;
     public void setDelegate(BookProgressFragmentDelegate delegate){ this.delegate = delegate;}
@@ -57,36 +57,68 @@ public class BookProgressFragment extends Fragment {
         if(book!=null){
             TextView bookName = (TextView) view.findViewById(R.id.bookProgressName);
             TextView bookAuthor = (TextView) view.findViewById(R.id.bookProgressAuthor);
-            bookPages = (TextView) view.findViewById(R.id.bookProgressPages);
             ImageView bookImage = (ImageView) view.findViewById(R.id.bookProgressImage);
             bookProgress = (ProgressBar) view.findViewById(R.id.progressBarBook);
+            bookProgressPages = (TextView) view.findViewById(R.id.bookProgressPages);
+
 
             bookName.setText(this.book.getBookName());
-            bookAuthor.setText(this.book.getAuthor());
+            bookAuthor.setText(" By " + this.book.getAuthor() + ". Pages: " + this.book.getPages());
             int pages = this.book.getPages();
             //bookPages.setText("Pages: " + pages);
         }
 
         list = (ListView) view.findViewById(R.id.bookReviewList);
+        Button update = (Button) view.findViewById(R.id.updateBookProgressBtn);
+        Button more = (Button) view.findViewById(R.id.othersReviewBtn);
+        bookProfileTitle = (TextView) view.findViewById(R.id.bookProfileTitle);
 
         if (curr != null) {
+            more.setVisibility(View.VISIBLE);
+            more.setText("More");
             Model.getInstance().getPostsByBookAndUserAsync(userId, book.getBookID(), new Model.GetPostsAsyncListener() {
                 @Override
                 public void onPostsArrived(ArrayList<Post> postArray, ArrayList<User> userArray, ArrayList<Book> bookArray) {
                     if (userArray == null && bookArray == null) {
+
                         int last_page = postArray.get(postArray.size() - 1).getCurrentPage();
                         data = postArray;
                         BookProgressAdapter adapter = new BookProgressAdapter();
                         list.setAdapter(adapter);
-                        bookPages.setText("Page " + last_page + " of " + book.getPages());
+                        bookProfileTitle.setText("My Progress ("+last_page+"/"+book.getPages()+")");
                         bookProgress.setMax(book.getPages());
                         bookProgress.setProgress(last_page);
                     }
                 }
             });
         }
+        else
+        {
+            more.setVisibility(View.GONE);
 
-        Button update = (Button) view.findViewById(R.id.updateBookProgressBtn);
+            update.setVisibility(View.GONE);
+            bookProgress.setVisibility(View.GONE);
+            bookProgressLayout = (RelativeLayout) view.findViewById(R.id.bookProgBar);
+            bookProgressLayout.setVisibility(View.GONE);
+            Model.getInstance().getBookReviewsAsync(book.getBookID(), new Model.GetBookReviewsAsyncListener() {
+                @Override
+                public void onPostsArrived(ArrayList<Post> postArray, ArrayList<User> userArray) {
+                    data = postArray;
+                    users = userArray;
+                    BookWorldProgressAdapter adapter = new BookWorldProgressAdapter();
+                    list.setAdapter(adapter);
+                    if (postArray.size() == 0) {
+                        bookProfileTitle.setText("There are no current reviews.");
+                    }
+                    else
+                    {
+                        bookProfileTitle.setText(postArray.size() + " Reviews of this book:");
+                    }
+                }
+            });
+        }
+
+
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,30 +174,13 @@ public class BookProgressFragment extends Fragment {
 
             if (convertView == null) {
                 LayoutInflater inflater = getActivity().getLayoutInflater();
-                if (curr != null)
-                {
-                    //Single user progressbar
-                    convertView = inflater.inflate(R.layout.book_progress_single_row, null);
-                }
-                else
-                {
-                    //Global Book Reviews
-                    convertView = inflater.inflate(R.layout.news_feed_single_row, null);
-                }
-
+                convertView = inflater.inflate(R.layout.book_progress_single_row, null);
             }
 
             final ImageView stars = (ImageView) convertView.findViewById(R.id.stars);
             final TextView pages = (TextView) convertView.findViewById(R.id.pageTextView);
             final TextView action = (TextView) convertView.findViewById(R.id.actionTextView);
             final TextView review = (TextView) convertView.findViewById(R.id.bookReview);
-
-
-
-
-
-
-
 
             if (curr != null)
             {
@@ -191,88 +206,116 @@ public class BookProgressFragment extends Fragment {
                 else
                 {
                     pages.setVisibility(View.GONE);
-                    title = "Started Rading";
+                    title = "Started Reading";
                     action.setText(title);
                     action.setVisibility(View.VISIBLE);
                 }
-
-
                 review.setText(post.getText());
                 stars.setImageResource(book.getStars(post.getGrade()));
                 stars.setVisibility(View.VISIBLE);
                 if (post.getText().equals("") && post.getGrade() == 0)
                     stars.setVisibility(View.GONE);
             }
-            else
-            {
+            return convertView;
+        }
+    }
 
 
-                final TextView bookName = (TextView) convertView.findViewById(R.id.bookName);
-                final TextView bookReview = (TextView) convertView.findViewById(R.id.bookReview);
-                final TextView userName = (TextView) convertView.findViewById(R.id.userProfileName);
-                final TextView page = (TextView) convertView.findViewById(R.id.pageTextView);
-                //action = (TextView) convertView.findViewById(R.id.actionTextView);
-                final TextView action2 = (TextView) convertView.findViewById(R.id.action2TextView);
-
-                //Global Book Reviews
-                Model.getInstance().getBookReviewsAsync(book.getBookID(), new Model.GetBookReviewsAsyncListener() {
-                    @Override
-                    public void onPostsArrived(ArrayList<Post> postArray, ArrayList<User> userArray) {
-
-                        data = postArray;
-                        BookProgressAdapter adapter = new BookProgressAdapter();
-                        list.setAdapter(adapter);
-                        bookProgressLayout.setVisibility(View.GONE);
-                        Post post = postArray.get(position);
-                        User user = userArray.get(position);
-                        userName.setText(user.getfName() + " " + user.getlName());
-                        userName.setTag(user);
-
-                        if (book.getBookName().length() > 27) {
-                            bookName.setText(book.getBookName().substring(0, 27) + "...");
-                        }
-                        else {
-                            bookName.setText(book.getBookName());
-                        }
-                        bookName.setTag(post);
 
 
-                        if (post.getCurrentPage() == 0 && post.getGrade() == 0) {
-                            action.setText("Started ");
-                            bookReview.setVisibility(View.GONE);
-                            action2.setText("Not yet rated.");
-                            page.setVisibility(View.GONE);
-                            stars.setVisibility(View.GONE);
-                            action2.setVisibility(View.GONE);
 
-                        }
-                        else
-                        {
-                            page.setVisibility(View.VISIBLE);
-                            page.setText(" Page: " + post.getCurrentPage());
-                            stars.setImageResource(book.getStars(post.getGrade()));
-                            stars.setVisibility(View.VISIBLE);
-                            action2.setVisibility(View.VISIBLE);
-                            action2.setText("Rated ");
-                            if (post.isFinished())
-                                action.setText("finished ");
-                            else
-                                action.setText("is reading ");
 
-                            if (post.getText().isEmpty())
-                            {
-                                bookReview.setVisibility(View.GONE);
-                            }
-                            else
-                            {
-                                bookReview.setText(post.getText());
-                                bookReview.setVisibility(View.VISIBLE);
-                            }
-                        }
-                    }
-                });
+
+    class BookWorldProgressAdapter extends BaseAdapter {
+
+        public BookWorldProgressAdapter() {
+        }
+
+        @Override
+        public int getCount() { //returns the size of the list
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) { //returns the post
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) { //returns post id
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                convertView = inflater.inflate(R.layout.news_feed_single_row, null);
+            }
+
+            final ImageView stars = (ImageView) convertView.findViewById(R.id.stars);
+            final TextView pages = (TextView) convertView.findViewById(R.id.pageTextView);
+            final TextView action = (TextView) convertView.findViewById(R.id.actionTextView);
+            final TextView review = (TextView) convertView.findViewById(R.id.bookReview);
+
+            final TextView bookName = (TextView) convertView.findViewById(R.id.bookName);
+            final TextView bookReview = (TextView) convertView.findViewById(R.id.bookReview);
+            final TextView userName = (TextView) convertView.findViewById(R.id.userProfileName);
+            final TextView page = (TextView) convertView.findViewById(R.id.pageTextView);
+            //action = (TextView) convertView.findViewById(R.id.actionTextView);
+            final TextView action2 = (TextView) convertView.findViewById(R.id.action2TextView);
+
+
+
+            Post post = data.get(position);
+            User user = users.get(position);
+            userName.setText(user.getfName() + " " + user.getlName());
+            userName.setTag(user);
+
+            if (book.getBookName().length() > 27) {
+                bookName.setText(book.getBookName().substring(0, 27) + "...");
+            }
+            else {
+                bookName.setText(book.getBookName());
+            }
+            bookName.setTag(post);
+
+
+            if (post.getCurrentPage() == 0 && post.getGrade() == 0) {
+                action.setText("Started ");
+                bookReview.setVisibility(View.GONE);
+                action2.setText("Not yet rated.");
+                page.setVisibility(View.GONE);
+                stars.setVisibility(View.GONE);
+                action2.setVisibility(View.GONE);
 
             }
+            else
+            {
+                page.setVisibility(View.VISIBLE);
+                page.setText(" Page: " + post.getCurrentPage());
+                stars.setImageResource(book.getStars(post.getGrade()));
+                stars.setVisibility(View.VISIBLE);
+                action2.setVisibility(View.VISIBLE);
+                action2.setText("Rated ");
+                if (post.isFinished())
+                    action.setText("finished ");
+                else
+                    action.setText("is reading ");
+
+                if (post.getText().isEmpty())
+                {
+                    bookReview.setVisibility(View.GONE);
+                }
+                else
+                {
+                    bookReview.setText(post.getText());
+                    bookReview.setVisibility(View.VISIBLE);
+                }
+            }
+
+
 
 
             return convertView;
