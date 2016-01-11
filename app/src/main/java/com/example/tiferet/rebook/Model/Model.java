4912103ea586664463dbba15854b56d42a1dbf9e
tifochen <tifcohen@gmail.com>
2016-01-11 12:04:53 +0200

    Merge branches 'bug_fixes' and 'master' of https://github.com/tifcohen/ReBook into bug_fixes
    # Conflicts:
    #	app/src/main/java/com/example/tiferet/rebook/Fragments/BookProgressFragment.java

--- app/src/main/java/com/example/tiferet/rebook/Model/Model.java
+++ app/src/main/java/com/example/tiferet/rebook/Model/Model.java
@@ -1,54 +1,69 @@
 package com.example.tiferet.rebook.Model;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Alon Abadi on 1/5/2016.
- */
+ **/
+
 public class Model {
     private final static Model instance = new Model();
     Context context;
-    //ModelSql model = new ModelSql();
-    ModelParse model = new ModelParse();
+    ModelSql modelSql;
+    ModelParse model;
 
-
-    private Model(){}
+    private Model(){
+        init(context);
+    }
 
     public static Model getInstance(){
         return instance;
     }
 
     public void init(Context context){
-        this.context = context;
+        model = new ModelParse();
         model.init(context);
+        modelSql = new ModelSql(context);
     }
+
+/*    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }*/
 
     public void updateReadStatus(final Post post){ model.updateReadStatus(post);}
 
     public interface GetBooksListener{
         void onBooksArrived(ArrayList<Book> books);
     }
+    //Parse
     public void getAllBooks(GetBooksListener listener){
         model.getAllBooks(listener);
     }
+    //SQL
+    public List<Book>getAllBooks(){
+        return modelSql.getAllBooks();
+    }
 
-    public void addPost(Post post){ model.addPost(post);}
+    //Parse + SQL
+    public void addPost(Post post){
+       /* if(isNetworkAvailable()) {
+            model.addPost(post);
+        }*/
+        model.addPost(post);
+        modelSql.addPost(post);
+    }
 
     public void addBook(Book book){
         model.addBook(book);
+        modelSql.addBook(book);
     }
 
     public void addBookToUser(Book book){ model.addBookToUser(book);}
@@ -111,7 +126,7 @@ public class Model {
     }
 
     public void getBookByIdAsync(String id, GetBookListener listener){
-        model.getBookByIdAsync(id, listener);
+        model.getBookByIdAsync(id,listener);
     }
 
     public void getUserByIdAsync(String id, GetUserListener listener){
@@ -142,73 +157,4 @@ public class Model {
     public void getFollowingListByIdAsync(String id, GetFollowingListener listener) {
         model.getFollowingListByIdAsync(id, listener);
     }
-
-    public void saveImage(final Bitmap imageBitmap, final String imageName) {
-        //saveImageToFile(imageBitmap,imageName); // synchronously save image locally
-        Thread d = new Thread(new Runnable() {  // asynchronously save image to parse
-            @Override
-            public void run() {
-                model.saveImage(imageBitmap,imageName);
-            }
-        });
-        d.start();
-    }
-
-    public interface LoadImageListener{
-        public void onResult(Bitmap imageBmp);
-    }
-
-    public void loadImage(final String imageName, final LoadImageListener listener) {
-        AsyncTask<String,String,Bitmap> task = new AsyncTask<String, String, Bitmap >() {
-            @Override
-            protected Bitmap doInBackground(String... params) {
-                Bitmap bmp = loadImageFromFile(imageName);              //first try to fin the image on the device
-                if (bmp == null) {                                      //if image not found - try downloading it from parse
-                    bmp = model.loadImage(imageName);
-                    //if (bmp != null) saveImageToFile(bmp,imageName);    //save the image locally for next time
-                }
-                return bmp;
-            }
-
-            @Override
-            protected void onPostExecute(Bitmap result) {
-                listener.onResult(result);
-            }
-        };
-        task.execute();
-    }
-
-    private void saveImageToFile(Bitmap imageBitmap, String imageFileName){
-        FileOutputStream fos;
-        OutputStream out = null;
-        try {
-            File dir = context.getExternalFilesDir(null);
-            out = new FileOutputStream(new File(dir,imageFileName));
-            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Bitmap loadImageFromFile(String fileName){
-        String str = null;
-        Bitmap bitmap = null;
-        try {
-            File dir = context.getExternalFilesDir(null);
-            InputStream inputStream = new FileInputStream(new File(dir,fileName));
-            bitmap = BitmapFactory.decodeStream(inputStream);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-
-
-
-
-
 }
